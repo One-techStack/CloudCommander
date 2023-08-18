88fc9a2062a1e06db36aed5f6b3f08d35ee29e7f
Sebastian Varga <sebastian@tech-stack.co>
2023-08-18 14:58:22 +0200

    Changes for GCP

--- gcp/gcp_account_info.py
+++ gcp/gcp_account_info.py
@@ -1,52 +1,31 @@
-# from google.oauth2 import credentials, service_account
-# from google.cloud import resourcemanager
-# import google.auth
-
-# def get_gcp_account_info():
-#     try:
-#         # Authenticate and initialize Cloud Resource Manager
-#         creds, project_id = google.auth.default()
-#         client = resourcemanager.ProjectsClient(credentials=creds)
-
-#         # Fetch account information
-#         project = client.get_project(project_id)
-#         account_info = {
-#             'Project ID': project.project_id,
-#             'Project Name': project.name,
-#             'Project Number': project.number,
-#             'Lifecycle State': project.state.name,
-#             'Project Labels': project.labels
-#         }
-
-#         return account_info
-#     except Exception as e:
-#         print(f"Failed to fetch GCP account information. Reason: {e}")
-#         return None
-
-# if __name__ == "__main__":
-#     info = get_gcp_account_info()
-#     print(info)
-
 from google.cloud import resourcemanager
-import google.auth
+from google.oauth2 import service_account
 
 def get_gcp_account_info():
     try:
-        # Authenticate with GCP
-        creds, project_id = google.auth.default()
+        # get the project id
+        def default():
+            with open('') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if line.startswith('project'):
+                        project_id = line.split('=')[1].strip()
+                        return project_id
+                else:
+                    return None
+                
+        project_id = default()
         
+        # Authenticate with GCP using your service account key file
         # Initialize the Resource Manager Client
-        client = resourcemanager.ProjectsClient(credentials=creds)
+        client = resourcemanager.ProjectsClient(credentials=credentials)
         
         # Get the project details
         project = client.get_project(project_id)
         
         account_info = {
             'Project ID': project.project_id,
-            'Project Name': project.name,
-            # 'Project Number': project.number,
-            # 'Lifecycle State': project.status,
-            'Project Labels': project.labels
+            'Project Name': project.name
         }
 
         return account_info
